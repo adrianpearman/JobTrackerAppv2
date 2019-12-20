8973ad3c367374eaf6efd9b4a015797bf26670f9
Adrian Pearman <adrianpearman12@gmail.com>
2019-12-20 18:37:08 -0400

    updated server file with new config values.

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const MONGO_URI = require("./config/config.js");
 const apiRoutes = require("./routes/apiRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 // This is the rendered build index.html from the client build folder
-const root = require("path").join(__dirname, "../client", "build");
+const clientRoot = require("path").join(__dirname, "../client", "build");
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,14 +22,18 @@ app.use(cors());
 
 // Connecting to Database
 if (process.env.NODE_ENV === "production") {
-  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, err => {
-    if (err) {
-      console.error(err);
+  mongoose.connect(
+    process.env.MONGO_URI.PROD,
+    { useNewUrlParser: true },
+    err => {
+      if (err) {
+        console.error(err);
+      }
+      console.log("Connected to Prodcution Database");
     }
-    console.log("Connected to Prodcution Database");
-  });
+  );
 } else {
-  mongoose.connect(MONGO_URI, { useNewUrlParser: true }, err => {
+  mongoose.connect(MONGO_URI.DEV, { useNewUrlParser: true }, err => {
     if (err) {
       console.error(err);
     }
@@ -41,11 +45,11 @@ if (process.env.NODE_ENV === "production") {
 app.use(apiRoutes);
 app.use(userRoutes);
 
-// Render root buildfile if the application is ona production server
+// Render client root buildfile if the application is ona production server
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(root));
+  app.use(express.static(clientRoot));
   app.get("/", (req, res) => {
-    res.sendFile("index.html", { root });
+    res.sendFile("index.html", { clientRoot });
   });
 }
 
