26aaa3576642e8b3a7f4fdeaa33d907fbe6cce05
Adrian Pearman <adrianpearman12@gmail.com>
2019-12-20 18:36:11 -0400

    updated config fil structure for dev and prod environments.

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,3 +1,6 @@
-const MONGOURI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds161391.mlab.com:61391/job-app-tracker-dev`;
+const MONGOURI = {
+  DEV: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds161391.mlab.com:61391/job-app-tracker-dev`,
+  PROD: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds161391.mlab.com:61391/job-app-tracker-prod`
+};
 
 module.exports = MONGOURI;
