1067eb11dea6f86bb921b65ce3ae793907b68e64
Adrian Pearman <adrianpearman12@gmail.com>
2020-02-24 01:48:13 -0500

    adding new logic to logged in middleware.

--- backend/middlewares/isLoggedIn.js
+++ backend/middlewares/isLoggedIn.js
@@ -1,6 +1,17 @@
+const jwt = require("jsonwebtoken");
+const keys = require("../config/keys");
+
 module.exports = (req, res, next) => {
-  if (!req.user) {
-    res.status(401).send({ error: "You must be logged in" });
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ error: "Access Denied: You must be logged in" });
   }
-  next();
+
+  try {
+    const verified = jwt.verify(token, keys.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).send("Invalid Token");
+  }
 };
