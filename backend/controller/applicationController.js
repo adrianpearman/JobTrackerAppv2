0c66292fda940437fa690d8daa6a549446aa664a
Adrian Pearman <adrianpearman12@gmail.com>
2020-04-07 01:07:18 -0400

    additional refactoring of server controllers.

--- backend/controller/applicationController.js
+++ backend/controller/applicationController.js
@@ -1,38 +1,28 @@
-const moment = require("moment");
-const Jobs = require("../models/jobModel.js");
+const Applications = require("../models/applicationModel.js");
 const User = require("../models/userModel");
+const unixTime = require("../utils/unixTime");
+const countEntryStatus = require("../utils/countEntryStatus");
+const countMonthlyEntries = require("../utils/countMonthlyEntries");
 
 // setting the object to be saved in the database
 let newJobObject = ({
-  applicationLink,
-  applicationMonth,
-  applicationPlatform,
-  applicationSource,
-  applicationYear,
   companyName,
-  hired,
-  hiredDate,
+  applicationLink,
+  applicationPlatform,
+  applicationMonth,
+  applicationYear,
   interview,
-  lastDayWorked,
   response,
   userId
 }) => {
   return {
-    applicationDate:
-      moment(
-        `${applicationYear}.${applicationMonth + 1}.01`,
-        "YYYY.MM.DD"
-      ).unix() * 1000,
-    applicationLink: applicationLink,
-    applicationMonth: applicationMonth + 1,
-    applicationPlatform: applicationPlatform,
-    applicationSource: applicationSource,
-    applicationYear: applicationYear,
     companyName: companyName,
-    hired: hired,
-    hiredDate: hiredDate,
+    applicationLink: applicationLink,
+    applicationPlatform: applicationPlatform,
+    applicationMonth: parseInt(applicationMonth),
+    applicationYear: parseInt(applicationYear),
+    applicationDate: unixTime(applicationMonth, applicationYear),
     interview: interview,
-    lastDayWorked: lastDayWorked,
     response: response,
     userId: userId
   };
@@ -41,107 +31,25 @@ let newJobObject = ({
 const applicationController = {
   getInitApplications: async (req, res) => {
     try {
-      // Base Variables
-      let dataContainer = [];
-      let month = new Date().getMonth() + 1;
-      let year = new Date().getFullYear();
-      let unix = moment(`${year}.${month}.01`, "YYYY.MM.DD").unix();
-      let baseUnix = moment(`2017.05.01`, "YYYY.MM.DD").unix();
-
       //All applicants
-      let applications = await Jobs.find({});
+      let applications = await Applications.find({});
       // Number of applications
       let applicationsTotalNumber = applications.length;
       // get last 10 applications
-      let applicationsLast10 = await Jobs.find({})
+      let applicationsLast10 = await Applications.find({})
         .sort({ appliationDate: -1 })
         .limit(10);
 
-      // Count all the application response values
-      const countApplicationStatus = arr => {
-        let appStatus0 = 0;
-        let appStatus1 = 0;
-        let appStatus2 = 0;
-        let appStatus3 = 0;
+      let applicationResponses = countEntryStatus(applications, false);
+      let applicationsPerMonth = countMonthlyEntries(applications, false);
 
-        for (let i = 0; i < arr.length; i++) {
-          if (arr[i].response === 0) {
-            appStatus0++;
-          } else if (arr[i].response === 1) {
-            appStatus1++;
-          } else if (arr[i].response === 2) {
-            appStatus2++;
-          } else if (arr[i].response === 3) {
-            appStatus3++;
-          }
-        }
-
-        return [
-          {
-            status: "Not Viewed",
-            applications: appStatus0
-          },
-          {
-            status: "Application Viewed",
-            applications: appStatus1
-          },
-          {
-            status: "Application Declined",
-            applications: appStatus2
-          },
-          {
-            status: "Offer Provided",
-            applications: appStatus3
-          }
-        ];
-      };
-      //  Count all of the monthly applications
-      const countMonthlyApplications = arr => {
-        let applicationMonths = {};
-        // Initialize Application Month
-        const initApplicationMonth = () => {
-          if (unix > baseUnix) {
-            unix = moment(`${year}.${month}.01`, "YYYY.MM.DD").unix();
-            applicationMonths[unix] = 0;
-            month--;
-            if (month === 0) {
-              month = 12;
-              year = year - 1;
-            }
-            return initApplicationMonth();
-          } else {
-            return;
-          }
-        };
-        initApplicationMonth();
-        //Seperate all of the values by the application date
-        for (let i = 0; i < arr.length; i++) {
-          if (!applicationMonths[arr[i].applicationDate]) {
-            applicationMonths[arr[i].applicationDate] = 1;
-          } else {
-            applicationMonths[arr[i].applicationDate]++;
-          }
-        }
-        // convert the value from an object to an array
-        for (const key in applicationMonths) {
-          if (applicationMonths.hasOwnProperty(key)) {
-            let data = {
-              date: new Date(parseInt(key) * 1000),
-              name: moment.unix(key).format("MMMM/YYYY"),
-              value: applicationMonths[key]
-            };
-            dataContainer.push(data);
-          }
-        }
-        return dataContainer;
-      };
       // grab latest results for current month
-      let currentMonthApplications = await Jobs.find({
-        applicationDate: unix
+      let currentMonthApplications = await Applications.find({
+        applicationDate: unixTime(
+          new Date().getMonth() + 1,
+          new Date().getFullYear()
+        )
       });
-
-      let applicationResponses = countApplicationStatus(applications);
-      let applicationsPerMonth = countMonthlyApplications(applications);
 
       let data = {
         applicationsTotalNumber,
@@ -152,36 +60,38 @@ const applicationController = {
       };
       return res.send(data);
     } catch (err) {
-      return res.status(400).send(err);
+      return res
+        .status(500)
+        .send({ err, errMsg: "Unable to process the request" });
     }
   },
   getAllApplications: async (req, res) => {
     try {
-      let applications = await Jobs.find({});
+      let applications = await Applications.find({});
       res.send({ applications });
     } catch (err) {
-      res.status(400).send({ errMsg: err });
+      res.status(500).send({ errMsg: err });
     }
   },
   getApplicationsPagination: async (req, res) => {
     const { startingPoint, amountOfRecords } = req.body;
 
     try {
-      let applications = await Jobs.find({})
+      let applications = await Applications.find({})
         .skip(startingPoint)
         .limit(amountOfRecords);
       res.send({ applications });
     } catch (err) {
-      res.status(400).send({ errMsg: err });
+      res.status(50).send({ errMsg: err });
     }
   },
   getSingleApplication: async (req, res) => {
-    const { jobId } = req.body;
+    const { jobId } = req.query;
 
-    let application = await Jobs.findById(jobId);
+    let application = await Applications.findById(jobId);
     //Validate whether job exists
     if (!application) {
-      return res.status(400).send({
+      return res.status(500).send({
         message: "no application found"
       });
     }
@@ -189,33 +99,53 @@ const applicationController = {
     try {
       res.send({ application });
     } catch (err) {
-      res.status(400).send({ errMsg: err });
+      res.status(500).send({ errMsg: err });
     }
   },
-  updateApplication: async (req, res) => {
-    const { userId, jobId } = req.body;
-    const updatedContent = req.body;
-
-    let user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(400).send({
-        message: "Please resubmit with a valid UserID",
-        errMsg: err
-      });
-    }
+  getApplicationsPerMonth: async (req, res) => {
+    const { month } = req.query;
 
     try {
-      await Jobs.updateOne({ _id: jobId }, updatedContent);
-      res.send({
-        message: `Successfully updated Jo ID: ${jobId}`,
-        updated: updatedContent
-      });
+      let applications = await Applications.find({ applicationMonth: month });
+      res.send({ applications });
     } catch (err) {
-      res.status(400).send({
-        message: "Error occured: Unable to find application",
-        errMsg: err
-      });
+      res.status(500).send(err);
+    }
+  },
+  getApplicationsPerYear: async (req, res) => {
+    const { year } = req.query;
+
+    try {
+      let applications = await Applications.find({ applicationYear: year });
+      res.send({ applications });
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  },
+  getApplicationsPerTimeFrame: async (req, res) => {
+    const { unixTime, timeFrame } = req.query;
+
+    try {
+      let applications;
+      if (timeFrame === "all time") {
+        applications = await Applications.find({});
+        return res.send({ applications });
+      } else {
+        applications = await Applications.find({ applicationDate: unixTime });
+        return res.send({ applications });
+      }
+    } catch (err) {
+      res.status(500).send({ errMsg: err });
+    }
+  },
+  getApplicationsFromCompany: async (req, res) => {
+    const { companyName } = req.query;
+
+    try {
+      let applications = await Applications.find({ companyName: companyName });
+      res.send({ applications });
+    } catch (err) {
+      res.status(500).send(err);
     }
   },
   addApplication: async (req, res) => {
@@ -226,25 +156,25 @@ const applicationController = {
 
     if (!user) {
       return res
-        .status(400)
+        .status(500)
         .send({ message: "Please resubmit with a valid UserID" });
     }
 
     //validating whether Job has been posted already
-    let job = await Jobs.findOne({ applicationLink });
+    let job = await Applications.findOne({ applicationLink });
 
     if (job) {
-      return res.status(400).send({
+      return res.status(500).send({
         message: "Unable to add to Database, Application already in System"
       });
     }
 
     try {
-      let newJobContent = new Jobs(newJobObject(req.body));
-      let newJob = await newJobContent.save();
-      res.send(newJob);
+      let newApplicationContent = new Applications(newJobObject(req.body));
+      let newApplication = await newApplicationContent.save();
+      res.send(newApplication);
     } catch (err) {
-      res.status(400).send({
+      res.status(500).send({
         message: "Error occured: Unable to create application",
         errMsg: err
       });
@@ -255,17 +185,42 @@ const applicationController = {
     let data = req.body.data;
     res.send(data);
   },
-  deleteApplication: async (req, res) => {
+  updateApplication: async (req, res) => {
     const { userId, jobId } = req.body;
+    const updatedContent = req.body;
 
-    let user = await User.find({ _id: userId });
+    let user = await User.findById(userId);
 
     if (!user) {
-      return res.status(400).send({ errMsg: err });
+      return res.status(500).send({
+        message: "Please resubmit with a valid UserID",
+        errMsg: err
+      });
     }
 
     try {
-      let job = await Jobs.deleteOne({ _id: jobId });
+      await Applications.updateOne({ _id: jobId }, updatedContent);
+      res.send({
+        message: `Successfully updated Jo ID: ${jobId}`,
+        updated: updatedContent
+      });
+    } catch (err) {
+      res.status(500).send({
+        message: "Error occured: Unable to find application",
+        errMsg: err
+      });
+    }
+  },
+  deleteApplication: async (req, res) => {
+    const { userId, jobId } = req.query;
+    const user = await User.find({ _id: userId });
+
+    if (!user) {
+      return res.status(500).send({ errMsg: err });
+    }
+
+    try {
+      const job = await Applications.deleteOne({ _id: jobId });
       res.send({
         ...job,
         jobId
@@ -274,36 +229,6 @@ const applicationController = {
       res.json({
         errMsg: err
       });
-    }
-  },
-  getApplicationsPerMonth: async (req, res) => {
-    const { month } = req.body;
-
-    try {
-      let applications = await Jobs.find({ applicationMonth: month });
-      res.send({ applications });
-    } catch (err) {
-      res.status(400).send(err);
-    }
-  },
-  getApplicationsPerYear: async (req, res) => {
-    const { year } = req.body;
-
-    try {
-      let applications = await Jobs.find({ applicationYear: year });
-      res.send({ applications });
-    } catch (err) {
-      res.status(400).send(err);
-    }
-  },
-  getApplicationsFromCompany: async (req, res) => {
-    const { companyName } = req.body;
-
-    try {
-      let applications = await Jobs.find({ companyName: companyName });
-      res.send({ applications });
-    } catch (err) {
-      res.status(400).send(err);
     }
   }
 };
