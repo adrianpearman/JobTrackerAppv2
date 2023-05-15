4d6943b6c9c082e60bcfd9584a92c6f10fdbdaeb
Adrian Pearman <adrianpearman12@gmail.com>
2023-05-15 12:31:13 -0400

    additional files from vite boilerplate

---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+// NPM Modules
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Components
+import App from "./Components/App.jsx";
+// Styling
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
