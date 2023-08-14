66e4a5c251db66b56f6fdf7e8f33cc6c82497671
Adrian Pearman <adrianpearman12@gmail.com>
2023-08-14 15:21:51 -0400

    updated mongo model for dynamic values.

--- backend/databases/mongo/models/analytic.js
+++ backend/databases/mongo/models/analytic.js
@@ -1,75 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const individualApplicationTypes = {
-  id: {
-    type: String,
+const AnalyticsModelSchema = new Schema(
+  {
+    averageAmountOfDailyApplications: { type: Number },
+    applicationsPerPlatform: Schema.Types.Mixed,
+    averageResponsesTimePerPlatform: Schema.Types.Mixed,
+    interviewsPerPlatform: Schema.Types.Mixed,
+    responsesPerPlatform: Schema.Types.Mixed,
+    totalApplications: { type: Number },
+    totalApplicationsWithInterview: { type: Number },
+    totalApplicationsWithResponse: { type: Number },
   },
-  uuid: {
-    type: String,
-  },
-  applicationDate: {
-    type: String,
-  },
-  decision: {
-    type: Boolean,
-  },
-  interview: {
-    type: String,
-  },
-  interviewDate: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
-  response: {
-    type: String,
-  },
-  responseDate: {
-    type: String,
-  },
-  sourceSite: {
-    type: String,
-  },
-  companyId: {
-    type: Number,
-  },
-  platformId: {
-    type: Number,
-  },
-  userId: {
-    type: String,
-  },
-};
-
-const AnalyticsModelSchema = new Schema({
-  applicationsPerPlatform: {
-    linkedin: {
-      type: Number,
-    },
-    indeed: {
-      type: Number,
-    },
-    internal: {
-      type: Number,
-    },
-  },
-  applicationsWithResponses: [individualApplicationTypes],
-  interviewsPerPlatform: {
-    linkedin: { type: Number },
-    indeed: { type: Number },
-    internal: { type: Number },
-  },
-  responsesPerPlatform: {
-    linkedin: { type: Number },
-    indeed: { type: Number },
-    internal: { type: Number },
-  },
-  totalApplications: { type: Number },
-  totalApplicationsWithInterview: { type: Number },
-  totalApplicationsWithResponse: { type: Number },
-});
+  { strict: false }
+);
 
 const AnalyticsModel = mongoose.model("AnalyticsModel", AnalyticsModelSchema);
 
