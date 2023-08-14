78b6c2fad9a022b84a8a1f7fc71974d1312793e7
Adrian Pearman <adrianpearman12@gmail.com>
2023-08-14 15:20:15 -0400

    refactored util functions to variables before exporting, created and improved analytic functions. created new functions for date values

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -16,147 +16,276 @@ const {
 // Mongo
 const AnalyticsModel = require("../databases/mongo/models/analytic");
 
-module.exports = {
-  // GENERAL UTIL FUNCTIONS
-  doesCompanyExist: async (name) => {
-    const company = await Company.findOne({
-      where: {
-        companyName: name,
+// Functions
+// GENERAL UTIL FUNCTIONS
+const convertToUnixSec = (time) => {
+  return parseInt((new Date(time).getTime() / 1000).toFixed(0));
+};
+const doesCompanyExist = async (name) => {
+  const company = await Company.findOne({
+    where: {
+      companyName: name,
+    },
+  });
+
+  if (company === null) {
+    return {
+      company: null,
+      exists: false,
+    };
+  } else {
+    return {
+      company: company.dataValues,
+      exists: true,
+    };
+  }
+};
+const daysBetweenApplications = (beginning, end) => {
+  const secondInDay = 86400;
+  const beginningUnix = parseInt(
+    (new Date(beginning).getTime() / 1000).toFixed(0)
+  );
+  const endUnix = parseInt((new Date(end).getTime() / 1000).toFixed(0));
+  return (endUnix - beginningUnix) / secondInDay;
+};
+// ANALYTICS UTIL FUNCTIONS
+const applicationAnalytics = async (userUuid) => {
+  try {
+    const user = await User.findOne({ where: { uuid: userUuid } });
+    const { analyticsUuid } = user.dataValues;
+
+    const userAnalytics = await AnalyticsModel.findOne({
+      _id: analyticsUuid,
+    });
+
+    return {
+      success: true,
+      analytics: userAnalytics,
+      msg: "",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      analytics: null,
+      msg: error.message || "An issue occured retrieving analytics",
+    };
+  }
+};
+const createUserApplicationAnalytics = async () => {
+  try {
+    const applicationsPerPlatform = {};
+    const interviewsPerPlatform = {};
+    const responsesPerPlatform = {};
+    let totalApplicationsWithInterview = 0;
+    let totalApplicationsWithResponse = 0;
+
+    const platforms = await Platform.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
       },
     });
 
-    if (company === null) {
-      return {
-        company: null,
-        exists: false,
-      };
-    } else {
-      return {
-        company: company.dataValues,
-        exists: true,
-      };
-    }
-  },
-  // ANALYTICS UTIL FUNCTIONS
-  applicationAnalytics: async (userUuid) => {
-    try {
-      const user = await User.findOne({ where: { uuid: userUuid } });
-      const { analyticsUuid } = user.dataValues;
+    platforms.forEach((p) => {
+      const { platformName } = p.dataValues;
+      applicationsPerPlatform[platformName] = 0;
+      interviewsPerPlatform[platformName] = 0;
+      responsesPerPlatform[platformName] = 0;
+    });
 
-      const userAnalytics = await AnalyticsModel.findOne({
-        _id: analyticsUuid,
-      });
+    const analytics = {
+      applicationsPerPlatform,
+      averageAmountOfDailyApplications,
+      averageResponsesTimePerPlatform,
+      interviewsPerPlatform,
+      responsesPerPlatform,
+      totalApplications: 0,
+      totalApplicationsWithInterview,
+      totalApplicationsWithResponse,
+    };
 
-      return {
-        success: true,
-        analytics: userAnalytics,
-        msg: "",
-      };
-    } catch (error) {
-      return {
-        success: false,
-        analytics: null,
-        msg: error.message || "An issue occured retrieving analytics",
-      };
-    }
-  },
-  createUserApplicationAnalytics: async () => {
-    try {
-      const applicationsPerPlatform = {};
-      const interviewsPerPlatform = {};
-      const responsesPerPlatform = {};
-      let totalApplicationsWithInterview = 0;
-      let totalApplicationsWithResponse = 0;
+    const analyticObj = new AnalyticsModel(analytics);
+    const newUserAnalyticObj = await analyticObj.save();
 
-      const platforms = await Platform.findAll({
-        attributes: {
-          exclude: ["createdAt", "updatedAt"],
-        },
-      });
+    return {
+      success: true,
+      analytics: newUserAnalyticObj,
+      msg: "",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      analytics: null,
+      msg: error.message || "damn",
+    };
+  }
+};
+const updateUserApplicationAnalytics = async (userUuid) => {
+  // Setting base obj
+  const baseAnalytics = {
+    applicationsPerPlatform: {},
+    averageAmountOfDailyApplications: 0,
+    averageResponsesTimePerPlatform: {},
+    interviewsPerPlatform: {},
+    responsesPerPlatform: {},
+    responsesTimeTotalPerPlatform: {},
+    totalApplications: 0,
+    totalApplicationsWithInterview: 0,
+    totalApplicationsWithResponse: 0,
+  };
 
-      platforms.forEach((p) => {
-        const { platformName } = p.dataValues;
-        applicationsPerPlatform[platformName] = 0;
-        interviewsPerPlatform[platformName] = 0;
-        responsesPerPlatform[platformName] = 0;
-      });
-
-      const analytics = {
+  try {
+    // Setting variable for earliest application
+    const arrayOfDates = [];
+    // Getting the user and their associated applications
+    const user = await User.findOne({
+      where: {
+        uuid: userUuid,
+      },
+      include: {
+        as: "applications",
+        model: Application,
+      },
+    });
+    // Getting all a vailable platforms
+    const platforms = await Platform.findAll({});
+    // Iterating through platforms to create base analytic objext
+    platforms.forEach((plat) => {
+      // Destructuring iterated platform object
+      const { platformName } = plat.dataValues;
+      // Destructuring base analytics object
+      const {
         applicationsPerPlatform,
-        applicationsWithResponses: [],
+        averageResponsesTimePerPlatform,
         interviewsPerPlatform,
         responsesPerPlatform,
-        totalApplications: 0,
-        totalApplicationsWithInterview,
-        totalApplicationsWithResponse,
-      };
-
-      const analyticObj = new AnalyticsModel(analytics);
-      const newUserAnalyticObj = await analyticObj.save();
-
-      return {
-        success: true,
-        analytics: newUserAnalyticObj,
-        msg: "",
-      };
-    } catch (error) {
-      return {
-        success: false,
-        analytics: null,
-        msg: error.message || "damn",
-      };
-    }
-  },
-  updateUserApplicationAnalytics: async (userUuid) => {
-    try {
-      const user = await User.findOne({
-        where: {
-          uuid: userUuid,
-        },
-        include: {
-          as: "applications",
-          model: Application,
-        },
+        responsesTimeTotalPerPlatform,
+      } = baseAnalytics;
+      // Setting base values
+      applicationsPerPlatform[platformName] = 0;
+      averageResponsesTimePerPlatform[platformName] = 0;
+      interviewsPerPlatform[platformName] = 0;
+      responsesPerPlatform[platformName] = 0;
+      responsesTimeTotalPerPlatform[platformName] = 0;
+    });
+    // Iterating thorough user applications
+    user.applications.forEach((ua) => {
+      // Destructured each user app
+      const { dataValues: apps } = ua;
+      // Pushing start dates to array for future calculation
+      arrayOfDates.push(apps.applicationDate);
+      // Looping through each platform
+      platforms.forEach((p) => {
+        const { dataValues: plat } = p;
+        // Segmenting the applications to their respective platforms
+        if (plat.id === apps.platformId) {
+          // Increasing the amount of applications per platform
+          baseAnalytics.applicationsPerPlatform[plat.platformName]++;
+          // If an application has an interview
+          if (apps.interview === true) {
+            baseAnalytics.interviewsPerPlatform[plat.platformName]++;
+          }
+          // If an application has been responded to
+          if (apps.response === true) {
+            // Destructuring base analytic object
+            const {
+              averageResponsesTimePerPlatform,
+              responsesPerPlatform,
+              responsesTimeTotalPerPlatform,
+            } = baseAnalytics;
+            // Destructring the individual application
+            const { applicationDate, responseDate } = apps;
+            // Increasing the total responses per platform
+            responsesPerPlatform[plat.platformName]++;
+            // Calculating the time between applying and and response datew
+            const t = daysBetweenApplications(applicationDate, responseDate);
+            // Increasing the total amount of days between response per platform
+            responsesTimeTotalPerPlatform[plat.platformName] =
+              responsesTimeTotalPerPlatform[plat.platformName] + t;
+            // Calculating the average days to respond to an application
+            averageResponsesTimePerPlatform[plat.platformName] = parseFloat(
+              (
+                responsesTimeTotalPerPlatform[plat.platformName] /
+                responsesPerPlatform[plat.platformName]
+              ).toFixed(2)
+            );
+          }
+        }
       });
-
-      const applications = user.applications.map((a) => a.dataValues);
-
-      console.log(applications);
-
-      return {
-        success: false,
-        analytics: {},
-        msg: "",
-      };
-    } catch (error) {
-      return {
-        success: false,
-        analytics: {},
-        msg: "",
-      };
-    }
-  },
-  deleteUserApplicationAnalytics: async (uuid) => {
-    try {
-      const analytics = await AnalyticsModel.deleteOne({ _id: uuid });
-
-      if (analytics.deletedCount === 0) {
-        throw new Error(`No Analytics matching UUID: ${uuid}`);
+      // If an application resulted in an interview; showcasing total amount of interviews
+      if (apps.interview === true) {
+        baseAnalytics.totalApplicationsWithInterview++;
       }
+      // If an application received a response; showcasing total amount of responses
+      if (apps.response === true) {
+        baseAnalytics.totalApplicationsWithResponse++;
+      }
+      // Increasing total amount of applications
+      baseAnalytics.totalApplications++;
+    });
+    // Calculating average applications per day
+    // Getting current date in seconds
+    const currentDate = new Date();
+    const firstApplicationDate = new Date(Math.min(...arrayOfDates));
+    // Calculating the average amount of applications per day
+    const averageDailyApplications =
+      baseAnalytics.totalApplications /
+      Math.floor(daysBetweenApplications(firstApplicationDate, currentDate));
+    // Setting value to analytic object
+    baseAnalytics.averageAmountOfDailyApplications = averageDailyApplications;
+    // Deleting un needed obj property
+    delete baseAnalytics.responsesTimeTotalPerPlatform;
+    // Updating individual user analytics
+    await AnalyticsModel.findOneAndUpdate(
+      { id: user.dataValues.platformId },
+      baseAnalytics
+    );
+    // Getting the newly created obj to return
+    const newAnalytics = await AnalyticsModel.findOne({
+      id: user.dataValues.platformId,
+    });
 
-      return {
-        success: true,
-        analyticsUuid: uuid,
-        msg: `Successdfully deleted UUID:${uuid}`,
-      };
-    } catch (error) {
-      return {
-        success: false,
-        analyticsUuid: null,
-        msg: error.message || "",
-      };
+    return {
+      success: true,
+      analytics: newAnalytics,
+      msg: "Successfully updated user analytics",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      analytics: {},
+      msg: error.message || "An error occured, plese try again later",
+    };
+  }
+};
+const deleteUserApplicationAnalytics = async (uuid) => {
+  try {
+    const analytics = await AnalyticsModel.deleteOne({ _id: uuid });
+
+    if (analytics.deletedCount === 0) {
+      throw new Error(`No Analytics matching UUID: ${uuid}`);
     }
-  },
+
+    return {
+      success: true,
+      analyticsUuid: uuid,
+      msg: `Successdfully deleted UUID:${uuid}`,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      analyticsUuid: null,
+      msg: error.message || "",
+    };
+  }
+};
+
+module.exports = {
+  doesCompanyExist,
+  daysBetweenApplications,
+  applicationAnalytics,
+  createUserApplicationAnalytics,
+  updateUserApplicationAnalytics,
+  deleteUserApplicationAnalytics,
   // DATABASE AND AUTH FUNCTIONS
   firebaseAdminAuth: () => {
     const firebaseAdminConfig = {
