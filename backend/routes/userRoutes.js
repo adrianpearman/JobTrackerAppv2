7f0b783fae3e5ba7865e1f7270f9faadd70a32b3
Adrian Pearman <adrianpearman12@gmail.com>
2023-09-04 14:08:05 -0400

    added new routes relating to user and user roles.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,18 +4,35 @@ const router = express.Router({ mergeParams: true });
 // Controller
 const {
   createUser,
+  createUserRole,
   deleteUser,
+  deleteUserRole,
   getUser,
+  getIndividualUserRole,
+  getUserRoles,
   updateUser,
 } = require("../controller/userController");
+// Middlewares
+const { checkAuthorized } = require("../middlewares");
 
+// User Routes
 // GET
-router.get("/api/user", getUser);
+router.get("/api/user", checkAuthorized, getUser);
 // POST
 router.post("/api/createUser", createUser);
 // PUT
-router.put("/api/updateUser", updateUser);
+router.put("/api/updateUser", checkAuthorized, updateUser);
 // DELETE
-router.delete("/api/deleteUser", deleteUser);
+router.delete("/api/deleteUser", checkAuthorized, deleteUser);
+
+// User Role Routes
+// GET
+router.get("/api/roles", checkAuthorized, getUserRoles);
+router.get("/api/role", checkAuthorized, getIndividualUserRole);
+// POST
+router.post("/api/createRole", checkAuthorized, createUserRole);
+// PUT
+// DELETE
+router.delete("/api/deleteRole", checkAuthorized, deleteUserRole);
 
 module.exports = router;
